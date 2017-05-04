959dc04b8fd17c421ee8b8a8dd1c65a7f37e28f1
Eric <ericfolland@gmail.com>
2017-05-04 09:54:16 -0700

    movement update

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -23,20 +23,17 @@ public class Ship : MonoBehaviour {
         keyboardV.Normalize();
         var targetV = mouseV + keyboardV;
         targetV.Normalize();
-        if (targetV.sqrMagnitude > 0f)
+        var targetDirection = -v.normalized * 0.4f + targetV * 0.60f;
+        var heading = targetDirection.normalized;
+        if (targetV.sqrMagnitude > 0f || v.sqrMagnitude > 0f)
         {
-            var normalizedV = v.normalized;
-            var targetDirection = (-normalizedV * 0.9f + targetV).normalized;
-            Debug.Log("targetDirection: " + targetDirection);
             var targetA = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
-            Debug.Log("targetA: " + targetA);
             angle = Mathf.MoveTowardsAngle(angle, targetA, rotationSpeed * Time.deltaTime);
             var euler = transform.localEulerAngles;
             euler.z = angle - 90f;
             transform.localEulerAngles = euler;
-            var heading = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)).normalized;
-            v += heading * Time.deltaTime * thrust;
         }
+        v += heading * Time.deltaTime * thrust;
         p += v * Time.deltaTime;
     }
 

0ae5323d8c583ed0d873e2edd6019ef8ac25137a
Eric <ericfolland@gmail.com>
2017-05-09 21:25:31 -0700

    movement working pretty well now

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Ship : MonoBehaviour {
     public Vector2 p, v;
-    float thrust = 1000f;
+    float maxThrust = 1000f;
     float angle = 0f;
     float rotationSpeed = 720f; // deg/sec
 
@@ -24,17 +24,25 @@ public class Ship : MonoBehaviour {
         var targetV = mouseV + keyboardV;
         targetV.Normalize();
         var targetDirection = -v.normalized * 0.4f + targetV * 0.60f;
-        var heading = targetDirection.normalized;
-        if (targetV.sqrMagnitude > 0f || v.sqrMagnitude > 0f)
+        var angleRad = angle * Mathf.Deg2Rad;
+        var targetA = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
+        angle = Mathf.MoveTowardsAngle(angle, targetA, rotationSpeed * Time.deltaTime);
+        var heading = targetDirection.normalized; // new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad)).normalized;
+        var vA = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+        var thrust = maxThrust; // (1f - Mathf.Clamp01(Mathf.DeltaAngle(angle, targetA) / 90f)) * maxThrust;
+        var dv = heading * Time.deltaTime * thrust;
+        // if slowing down, come to a stop rather than go past
+        if (targetV.sqrMagnitude < 0.1f && Mathf.DeltaAngle(targetA, vA) > 90 && v.sqrMagnitude < dv.sqrMagnitude)
+            dv = dv / dv.sqrMagnitude * v.sqrMagnitude;
+        v += dv;
+        p += v * Time.deltaTime;
+
+        if (dv.sqrMagnitude > 0.1f)
         {
-            var targetA = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
-            angle = Mathf.MoveTowardsAngle(angle, targetA, rotationSpeed * Time.deltaTime);
             var euler = transform.localEulerAngles;
             euler.z = angle - 90f;
             transform.localEulerAngles = euler;
         }
-        v += heading * Time.deltaTime * thrust;
-        p += v * Time.deltaTime;
     }
 
     // Update is called once per frame

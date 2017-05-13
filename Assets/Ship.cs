d3eb35519791b9d6e323fab2052d76f75e7c0e5c
Eric <ericfolland@gmail.com>
2017-05-12 20:55:32 -0700

    improved movement logic and added thrust fx

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -1,16 +1,17 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Linq;
+using System.Collections.Generic;
 
 public class Ship : Singleton<Ship> {
     public Vector2 p, v;
-    float maxThrust = 2000f;
+    public ParticleSystem thrustParticles;
+    float maxThrust = 4000f;
     float angle = 0f;
-    float rotationSpeed = 720f; // deg/sec
-    float slowDownThrustFactor = 2f;
-    float offAngleThrustCurve = 0.5f;
+    float rotationSpeed = 1080f; // deg/sec
+    float offAngleThrustCurve = 1.4f;
     float thrustSpeedFactorMax = 1000f;
-    float thrustSpeedFactorCurve = 3f;
-    float thrustSpeedFactorMin = 0.1f;
+    float thrustSpeedFactorCurve = 0.2f;
+    float thrustSpeedFactorMin = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -28,18 +29,18 @@ public class Ship : Singleton<Ship> {
         keyboardV.Normalize();
         var targetV = mouseV + keyboardV;
         targetV.Normalize();
-        var targetDirection = -v.normalized * 0.4f + targetV * 0.60f;
+        var targetDirection = -v.normalized * 0.4f + targetV * 0.6f;
         var angleRad = angle * Mathf.Deg2Rad;
         var targetA = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg;
         angle = Mathf.MoveTowardsAngle(angle, targetA, rotationSpeed * Time.deltaTime);
         var heading = targetDirection.normalized; // new Vector2(Mathf.Cos(angleRad), Mathf.Sin(angleRad)).normalized;
         var vA = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
         //var thrust = maxThrust; // (1f - Mathf.Clamp01(Mathf.DeltaAngle(angle, targetA) / 90f)) * maxThrust;
-        var slowDownInv = 1f / slowDownThrustFactor;
         var dA = Mathf.Clamp01(Mathf.Abs(Mathf.DeltaAngle(vA, targetA)) / 180f);
-        var angleFactor = (slowDownInv + Mathf.Pow(dA, offAngleThrustCurve) * (1f - slowDownInv));
+        var angleFactor = Mathf.Pow(dA, offAngleThrustCurve);
+        Debug.Log(angleFactor);
         var speedFactor = (1f - Mathf.Pow(Mathf.Clamp01(v.magnitude / thrustSpeedFactorMax), thrustSpeedFactorCurve)) * (1f - thrustSpeedFactorMin) + thrustSpeedFactorMin;
-        var thrust = speedFactor * angleFactor * maxThrust;
+        var thrust = (speedFactor + angleFactor) * 0.5f * maxThrust;
         var dv = heading * Time.deltaTime * thrust;
         // if slowing down, come to a stop rather than go past
         if (targetV.sqrMagnitude < 0.1f && Mathf.DeltaAngle(targetA, vA) > 90 && v.sqrMagnitude < dv.sqrMagnitude)
@@ -47,16 +48,27 @@ public class Ship : Singleton<Ship> {
         v += dv;
         p += v * Time.deltaTime;
 
-        if (dv.sqrMagnitude > 0.1f)
+        // angle
+        var thrustOn = dv.sqrMagnitude > 0.1f || v.sqrMagnitude > 0.1f;
+        if (thrustOn)
         {
             var euler = transform.localEulerAngles;
             euler.z = angle - 90f;
             transform.localEulerAngles = euler;
         }
+
+        // particles
+        var thrusterThrust = thrustOn ? dv.magnitude / Time.deltaTime : 0f;
+        var thrustFactor = thrustOn ? Mathf.Clamp01(thrusterThrust / (maxThrust * 10f)) : 0f;
+        thrustParticles.startSize = thrustFactor * 16f;
+        var tp = thrustParticles.transform.localPosition;
+        tp.y = -300f + thrustFactor * -10000f;
+        thrustParticles.transform.localPosition = tp;
+        thrustParticles.startSpeed = thrustFactor * 1600f;
     }
 
     // Update is called once per frame
     void Update () {
-        Main.Instance.bg.rectTransform.anchoredPosition = -p * 0.01f;
+        Main.Instance.bg.rectTransform.anchoredPosition = -p * 1f;
 	}
 }
